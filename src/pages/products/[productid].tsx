fdabfaaf2c5f9d7ae29e30a0ca4c83c59fdfc37f
Chanhwi Lee <chanhwi.lee6@gmail.com>
2023-03-22 21:52:52 +0900

    [UPDATE] db.json 업데이트

--- src/pages/products/[productid].tsx
+++ src/pages/products/[productid].tsx
@@ -1,4 +1,3 @@
-import { useRouter } from 'next/router';
 import React, { useEffect, useState } from 'react';
 import * as S from '@/components/stylecomponents/styles';
 import Image from 'next/image';
