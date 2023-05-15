51abf429fc86644136b2a9d013c7abbbacb4ba39
Chanhwi Lee <74127841+Strongorange@users.noreply.github.com>
2023-05-15 14:47:43 +0900

    [FIX] Socket.io transport : websocket (#77)

--- src/contexts/socket.ts
+++ src/contexts/socket.ts
@@ -3,5 +3,5 @@ import { createContext } from 'react';
 
 const SOCKET_URL = `${process.env.NEXT_PUBLIC_SERVER_BASE_URL}auction`;
 
-export const socket = io(`${SOCKET_URL}`);
+export const socket = io(`${SOCKET_URL}`, { transports: ['websocket'] });
 export const SocketContext = createContext(socket);
